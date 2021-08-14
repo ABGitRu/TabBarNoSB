ab8a39276ddda2b27d4b545de323f7bb9329a875
Alexey Barinov <begoodbebad2012@gmail.com>
2021-08-14 21:36:04 +0300

    init

--- TabBarNoSB/FirstViewControler.swift
+++ TabBarNoSB/FirstViewControler.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewControler.swift
 //  TabBarNoSB
 //
 //  Created by Mac on 14.08.2021.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewControler: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
---
+++ TabBarNoSB/SecondVC.swift
@@ -0,0 +1,17 @@
+//
+//  SecondVC.swift
+//  TabBarNoSB
+//
+//  Created by Mac on 14.08.2021.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+}
